1adabfe15964092ab70810d2d26fb1c629b1e5be
issam-zouiten <zouiten.issam@gmail.com>
2021-03-06 16:11:43 +0100

    container_complet_profile

--- client/src/containers/completeprofile/infos.js
+++ client/src/containers/completeprofile/infos.js
@@ -1,53 +1,79 @@
 import infos from '../../components/completeprofile/infos';
 import {connect} from "react-redux";
+import {reduxForm} from 'redux-form';
 import {step1_info} from '../../actions/InfosAction';
-import {reduxForm } from 'redux-form';
 
 const validate = (values) => {
-  const errors = {};
-  const requiredFields = [
-      'firstname',
-      'lastname',
-      'username',
-      'email',
-  ];
+    const errors = {};
+    const requiredFields = [
+        'gender',
+        'Sexual_orientation',
+        'date_birthday',
+        'biography',
+    ];
+    const requiredArr = [
+        'tags'
+    ];
+    requiredFields.forEach(field => {
+        if (!values[field] || !values[field].trim()) {
+            errors[field] = 'Required !';
+        }
+    });
+    requiredArr.forEach(field => {
+        if (!values[field]) {
+            errors[field] = 'Required !';
+        }
+    });
 
-  requiredFields.forEach(field => {
-      if (!values[field] || !values[field].trim()) {
-          errors[field] = 'Required !';
-      }
-  });
+    if(values.bio && !/^.{1,200}$/.test(values.bio))
+        errors.bio = 'maximum 200 characters';
 
-  if(values.username && !/^[a-z0-9_-]{2,20}$/.test(values.username))
-      errors.username = 'Username can contain 2-20 characters, letters (a-z), numbers, "_" and "-"';
-  if (values.email && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email))
-      errors.email = "Invalid email address !";
-  return errors;
+    if(values.birthday && !/([12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01]))/.test(values.birthday))
+        errors.birthday = 'Invalid date !';
+
+    
+    return errors;
 }
+
 const mapStateToProps = (state) => (
-  {
-      "form" : state.form,
-      "status" : state.register.registerStatus,
-      "err": state.register.error
-  });
-
-  const mapDispatchToProps = {
-    "step1_info": step1_info
-};
-
-const mergeProps = (stateProps, dispatchProps, otherProps)=> ({
-  ...stateProps,
-  ...dispatchProps,
-  ...otherProps,
-  "handleSubmit" : otherProps.handleSubmit((form)=>{
-      dispatchProps.step1_info(form);
-  })
+{
+    'values' : state.form.values,
+    'selectOptions': state.addInfo.selectOptions,
+    'selectLoading': state.addInfo.selectLoading,
+    'selectError' : state.addInfo.error,
+    'user': state.user,
 });
-const connectedInfoContainer = connect(mapStateToProps, mapDispatchToProps,mergeProps)(infos);
+const mapDispatchToProps = {
+    "step1_info": step1_info,
+};
+const mergeProps = (stateProps, dispatchProps, otherProps) => ({
+    ...stateProps,
+    ...dispatchProps,
+    ...otherProps,
 
-const InfosContainer = reduxForm({
-  form : "infos",
-  "destroyOnUnmount": true,  
-  validate,
-})(connectedInfoContainer);
-export default InfosContainer;
+    "handleSubmit" : otherProps.handleSubmit((values) => {
+        dispatchProps.step1_info(values, stateProps.user.id);
+    }),
+});
+
+const connectedAddInfoContainer = connect(mapStateToProps, mapDispatchToProps, mergeProps)(infos);
+
+let AddInfoContainer = reduxForm ({
+    form : "addInfo",
+    destroyOnUnmount: true,
+    validate,
+})(connectedAddInfoContainer);
+
+AddInfoContainer = connect(
+    state => ({
+        initialValues: {
+            gender: state.user.gender,
+            sexOrient: state.user.sexOrient,
+            birthday: state.user.birthday,
+            bio: state.user.bio,
+            interests: state.user.interests,
+        },
+    }),
+)(AddInfoContainer);
+
+export default AddInfoContainer;
