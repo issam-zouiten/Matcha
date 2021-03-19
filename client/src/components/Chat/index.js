a26b3ae950239723bbdcf72ff5930d3bbf5b6de7
issam-zouiten <zouiten.issam@gmail.com>
2021-03-19 15:01:49 +0100

    chat_code_comp

--- client/src/components/Chat/index.js
+++ client/src/components/Chat/index.js
@@ -1,44 +1,46 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './chat.css';
 import MyFlash from '../commun/flash'
 
 const Conversations = (props) => {
-    const {handleSelectConversation, selected, conversations} = props;
-    return(
+    const { handleSelectConversation, selected, conversations } = props;
+    return (
         <div id="sidepanel">
             <div id="contacts">
                 <ul>
                     {selected && conversations.map(item => (
-                        <li onClick={() => handleSelectConversation(item.id)} key={item.id} className={selected.id === item.id ? "contact active": "contact"}>
+                        <li onClick={() => handleSelectConversation(item.id)} key={item.id} className={selected.id === item.id ? "contact active" : "contact"}>
                             <div className="wrap">
                                 <span className={item.online === 1 ? "contact-status online" : "contact-status offline"}></span>
-                                    <img src={`http://localhost:3001/${item.path}`} alt={item.lastname} />
-                                    <div className="meta">
-                                        <p className="name">{item.firstname} {item.lastname}</p>
-                                    </div>
+                                <img src={`http://localhost:3001/${item.path}`} alt={item.lastname} />
+                                <div className="meta">
+                                    <p className="name">{item.firstname} {item.lastname}</p>
+                                </div>
                             </div>
                         </li>
+
                     ))
                     }
                 </ul>
+                <hr />
             </div>
         </div>
     );
 }
 
 const ConvTitle = (props) => {
-    const {selected} = props;
-    return(
+    const { selected } = props;
+    return (
         <div className="contact-profile">
-            <img src={`http://localhost:3001/${selected.path}`} alt={selected.lastname}/>
+            <img src={`http://localhost:3001/${selected.path}`} alt={selected.lastname} />
             <p>{selected.firstname} {selected.lastname}</p>
         </div>
     );
 }
 
 const Messages = (props) => {
-    const {selected} = props;
-    return(
+    const { selected } = props;
+    return (
         <div className="messages">
             <ul>
                 {selected.messages && selected.messages.length > 0 && selected.messages.map(item => (
@@ -53,7 +55,7 @@ const Messages = (props) => {
 }
 
 const SendMessage = (props) => {
-    const {handleSendMessage, selected, dis} = props;
+    const { handleSendMessage, selected, dis } = props;
     const [message, setMessage] = useState("");
     const handleChange = e => setMessage(e.target.value);
     const handleSubmit = form => {
@@ -68,11 +70,11 @@ const SendMessage = (props) => {
         }
         document.querySelector('.message-input input').value = null;
     }
-    return(
+    return (
         <div className="message-input">
             <div className="wrap">
                 <form onSubmit={handleSubmit}>
-                    <input onChange={handleChange} className="text-msg" type="text" placeholder="Write your message..." disabled={dis}/>
+                    <input onChange={handleChange} className="text-msg" type="text" placeholder="Write your message..." disabled={dis} />
                     <button type="submit" className="submit">Send</button>
                 </form>
             </div>
@@ -81,15 +83,15 @@ const SendMessage = (props) => {
 }
 
 const Content = (props) => {
-    const {handleSendMessage, selected, dis} = props;
-    return(
+    const { handleSendMessage, selected, dis } = props;
+    return (
         <div className="content">
             {Object.keys(selected).length > 0 ? (
-            <>
-                <ConvTitle selected={selected}/>
-                <Messages selected={selected}/>
-                <SendMessage dis={dis} handleSendMessage={handleSendMessage} selected={selected}/>
-            </> ) :
+                <>
+                    <ConvTitle selected={selected} />
+                    <Messages selected={selected} />
+                    <SendMessage dis={dis} handleSendMessage={handleSendMessage} selected={selected} />
+                </>) :
                 <p id="selectCon">Select a conversation</p>
             }
         </div>
@@ -97,21 +99,21 @@ const Content = (props) => {
 }
 
 const Chat = (props) => {
-    const {handleSelectConversation, err, handleSendMessage, selected, conversations} = props;
+    const { handleSelectConversation, err, handleSendMessage, selected, conversations } = props;
     let disable = false;
-    if(err)
+    if (err)
         disable = true;
-    return(
-    <div id="frame">
-        {err && <MyFlash variant="error" msg={[err]}/>}
-        {conversations.length > 0 ? (
-        <>
-            <Conversations handleSelectConversation={handleSelectConversation} selected={selected} conversations={conversations}/>
-            <Content dis={disable} handleSendMessage={handleSendMessage} selected={selected} conversations={conversations}/>
-        </> ) : 
-            <p id="noMatches">No matches</p>
-        }
-    </div>
+    return (
+        <div id="frame">
+            {err && <MyFlash variant="error" msg={[err]} />}
+            {conversations.length > 0 ? (
+                <>
+                    <Conversations handleSelectConversation={handleSelectConversation} selected={selected} conversations={conversations} />
+                    <Content dis={disable} handleSendMessage={handleSendMessage} selected={selected} conversations={conversations} />
+                </>) :
+                <p id="noMatches">No matches</p>
+            }
+        </div>
     );
 }
 
