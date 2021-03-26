03acd23d36a69c7bdc91fb50a4e88ad405f25173
issam-zouiten <zouiten.issam@gmail.com>
2021-03-26 01:23:55 +0100

    Activite_frontend__

--- client/src/components/Activity/index.js
+++ client/src/components/Activity/index.js
@@ -8,8 +8,7 @@ import FavoriteIcon from '@material-ui/icons/Favorite';
 import IconButton from '@material-ui/core/IconButton';
 import Tooltip from '@material-ui/core/Tooltip';
 import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
-import SupervisedUserCircleOutlinedIcon from '@material-ui/icons/SupervisedUserCircleOutlined';
-import SupervisedUserCircleRoundedIcon from '@material-ui/icons/SupervisedUserCircleRounded';
+import CancelIcon from '@material-ui/icons/Cancel';
 import BlockIcon from '@material-ui/icons/Block';
 import PropTypes from 'prop-types';
 import Tabs from '@material-ui/core/Tabs';
@@ -17,7 +16,8 @@ import Tab from '@material-ui/core/Tab';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
 import { Grid } from '@material-ui/core';
-
+import * as Core from "@material-ui/core";
+import LoyaltyIcon from '@material-ui/icons/Loyalty';
 
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
@@ -31,7 +31,7 @@ function TabPanel(props) {
       aria-labelledby={`simple-tab-${index}`}
       {...other}
     >
-      {value === index && <Box p={3}>{children}</Box>}
+      {value === index && <Box style={{color:'#FFF'}} p={3}>{children}</Box>}
     </Typography>
   );
 }
@@ -50,12 +50,21 @@ function a11yProps(index) {
 }
 
 const useStyles = makeStyles((theme) => ({
+  paper: {
+    width:615,
+    justifyContent: 'center',
+    border: "1px solid #D7D4D3",
+    borderRadius: '8px',
+    marginTop: '145px',
+    background: "linear-gradient(30deg, #34ada4 10%, #0b777d 90%)",
+  },
+
   root: {
     flexGrow: 1,
     backgroundColor: theme.palette.background.paper,
     display: 'flex',
     height: 200,
-    width: 400,
+    width: 800,
   },
   margiin: {
     width: 200,
@@ -63,7 +72,7 @@ const useStyles = makeStyles((theme) => ({
   tabs: {
     width: 300,
   },
-  
+
 
 }));
 
@@ -77,137 +86,124 @@ const Activity = (props) => {
   };
 
   return (
-    <Grid container justify="center">
-      <Tabs
-        orientation="vertical"
-        value={value}
-        onChange={handleChange}
-        aria-label="Vertical tabs example"
-        className={classes.tabs}
-      >
-        <Tab label="You blocked" {...a11yProps(0)} />
-        <Tab label="You like" {...a11yProps(1)} />
-        <Tab label="You're liked by" {...a11yProps(2)} />
-        <Tab label="Profile views" {...a11yProps(3)} />
-      </Tabs>
-      <TabPanel value={value} index={0}>
-        <List >
-          {/* <Typography component="h1" variant="h4" align="center" color='primary'>
-         You blocked
-     </Typography> */}
-          {blockList.isUsers === true && blockList.users.map((value) => (
-            <ListItem key={value.id} button>
-              {/* <ListItemAvatar>
-            <Avatar
-            className={classes.large}
-              alt='Avatar'
-              src={`http://localhost:3001/images/${value.profilePic}`}
-            />
-          </ListItemAvatar> */}
-              <ListItemText className={classes.margiin}  id={value.id} >{value.firstname + '  ' + value.lastname}</ListItemText>
-              <ListItemSecondaryAction>
-                <Tooltip title="Unblock"><IconButton aria-label="unblock" onClick={(e) => handleDeblock(value.id)}>
-                  <BlockIcon color="secondary" />
-                </IconButton></Tooltip>
-              </ListItemSecondaryAction>
-            </ListItem>
-          ))}
-        </List>
-      </TabPanel>
-      <TabPanel value={value} index={1}>
-        <List >
-          {likeList.isUsers === true && likeList.users.map((value) => (
-            <ListItem key={value.id} button>
-              {/* <ListItemAvatar>
+    <Grid container
+    justify="center"
+    alignItems='center'
+    >
+      <Core.Paper
+        className={classes.paper}>
+        <Grid container >
+          <Tabs
+            orientation="vertical"
+            value={value}
+            onChange={handleChange}
+            className={classes.tabs}
+            style={{backgroundColor:'#FFF'}}
+          >
+            <Tab label="You blocked" {...a11yProps(0)} />
+            <Tab label="You like" {...a11yProps(1)} />
+            <Tab label="You're liked by" {...a11yProps(2)} />
+            <Tab label="Profile views" {...a11yProps(3)} />
+          </Tabs>
+          <TabPanel value={value} index={0}>
+            <List >
+              {blockList.isUsers === true && blockList.users.map((value) => (
+                <ListItem key={value.id} button >
+                  <ListItemText className={classes.margiin} id={value.id} >{value.firstname + '  ' + value.lastname}</ListItemText>
+                  <ListItemSecondaryAction>
+                    <Tooltip title="Unblock"><IconButton aria-label="unblock" onClick={(e) => handleDeblock(value.id)}>
+                      <BlockIcon color="secondary" />
+                    </IconButton></Tooltip>
+                  </ListItemSecondaryAction>
+                </ListItem>
+              ))}
+            </List>
+          </TabPanel>
+          <TabPanel value={value} index={1}>
+            <List >
+              {likeList.isUsers === true && likeList.users.map((value) => (
+                <ListItem key={value.id} button>
+               
+                  <ListItemText className={classes.margiin} id={value.id} >{value.firstname + '  ' + value.lastname}</ListItemText>
+                  <ListItemSecondaryAction >
+                    {value.like === 'iLike' &&
+                      <Tooltip title="Unlike"><IconButton aria-label="Unlike" onClick={(e) => handleDislike(value.id)}>
+                        <FavoriteIcon color="secondary" />
+                      </IconButton></Tooltip>
+                    }
+                    {value.like === 'match' &&
+                      <Tooltip title="Unmatch"><IconButton aria-label="Unmatch" onClick={(e) => handleDislike(value.id)}>
+                        <CancelIcon style={{color: '#FFF'}}  />
+                      </IconButton></Tooltip>
+                    }
+                  </ListItemSecondaryAction>
+                </ListItem>
+              ))}
+            </List>
+          </TabPanel>
+          <TabPanel value={value} index={2}>
+            <List >
+              {likedByList.isUsers === true && likedByList.users.map((value) => (
+                <ListItem key={value.id} button>
+                  {/* <ListItemAvatar>
                <Avatar
                className={classes.large}
                  alt='Avatar'
                  src={`http://localhost:3001/images/${value.profilePic}`}
                />
              </ListItemAvatar> */}
-              <ListItemText className={classes.margiin} id={value.id} >{value.firstname + '  ' + value.lastname}</ListItemText>
-              <ListItemSecondaryAction >
-                {value.like === 'iLike' &&
-                  <Tooltip title="Unlike"><IconButton aria-label="Unlike" onClick={(e) => handleDislike(value.id)}>
-                    <FavoriteIcon color="secondary" />
-                  </IconButton></Tooltip>
-                }
-                {value.like === 'match' &&
-                  <Tooltip title="Unmatch"><IconButton aria-label="Unmatch" onClick={(e) => handleDislike(value.id)}>
-                    <SupervisedUserCircleRoundedIcon color="primary" />
-                  </IconButton></Tooltip>
-                }
-              </ListItemSecondaryAction>
-            </ListItem>
-          ))}
-        </List>
-      </TabPanel>
-      <TabPanel value={value} index={2}>
-        <List >
-          {likedByList.isUsers === true && likedByList.users.map((value) => (
-            <ListItem key={value.id} button>
-              {/* <ListItemAvatar>
-               <Avatar
-               className={classes.large}
-                 alt='Avatar'
-                 src={`http://localhost:3001/images/${value.profilePic}`}
-               />
-             </ListItemAvatar> */}
-              <ListItemText  className={classes.margiin} id={value.id} >{value.firstname + ' ' + value.lastname}</ListItemText>
-              <ListItemSecondaryAction>
-                {value.like === 'heLiked' &&
-                  <Tooltip title="Like back"><IconButton aria-label="Like back" onClick={(e) => handleLike(value.id)}>
-                    <SupervisedUserCircleOutlinedIcon color="primary" />
-                  </IconButton></Tooltip>
-                }
-                {value.like === 'match' &&
-                  <Tooltip title="Unmatch"><IconButton aria-label="Unmatch" onClick={(e) => handleDislike(value.id)}>
-                    <SupervisedUserCircleRoundedIcon color="primary" />
-                  </IconButton></Tooltip>
-                }
-              </ListItemSecondaryAction>
-            </ListItem>
-          ))}
-        </List>
-      </TabPanel>
-      <TabPanel value={value} index={3}>
-        <List>
-          {viewProfileList.isUsers === true && viewProfileList.users.map((value) => (
-            <ListItem key={value.id} button>
-              {/* <ListItemAvatar>
-               <Avatar
-               className={classes.large}
-                 alt='Avatar'
-                 src={`http://localhost:3001/images/${value.profilePic}`}
-               />
-             </ListItemAvatar> */}
-              <ListItemText className={classes.margiin}  id={value.id} >{value.firstname + '  ' + value.lastname}</ListItemText>
-              <ListItemSecondaryAction>
-                {value.like === null &&
-                  <Tooltip title="Like"><IconButton aria-label="Like" onClick={(e) => handleLike(value.id)}>
-                    <FavoriteBorderIcon color="secondary" />
-                  </IconButton></Tooltip>
-                }
-                {value.like === 'iLike' &&
-                  <Tooltip title="Unlike"><IconButton aria-label="Unlike" onClick={(e) => handleDislike(value.id)}>
-                    <FavoriteIcon color="secondary" />
-                  </IconButton></Tooltip>
-                }
-                {value.like === 'heLiked' &&
-                  <Tooltip title="Like back"><IconButton aria-label="Like back" onClick={(e) => handleLike(value.id)}>
-                    <SupervisedUserCircleOutlinedIcon color="primary" />
-                  </IconButton></Tooltip>
-                }
-                {value.like === 'match' &&
-                  <Tooltip title="Unmatch"><IconButton aria-label="Unmatch" onClick={(e) => handleDislike(value.id)}>
-                    <SupervisedUserCircleRoundedIcon color="primary" />
-                  </IconButton></Tooltip>
-                }
-              </ListItemSecondaryAction>
-            </ListItem>
-          ))}
-        </List>
-      </TabPanel>
-    </Grid>);
+                  <ListItemText className={classes.margiin} id={value.id} >{value.firstname + ' ' + value.lastname}</ListItemText>
+                  <ListItemSecondaryAction>
+                    {value.like === 'heLiked' &&
+                      <Tooltip title="Like back"><IconButton aria-label="Like back" onClick={(e) => handleLike(value.id)}>
+                        <LoyaltyIcon style={{color: '#FFF'}} />
+                      </IconButton></Tooltip>
+                    }
+                    {value.like === 'match' &&
+                      <Tooltip title="Unmatch"><IconButton aria-label="Unmatch" onClick={(e) => handleDislike(value.id)}>
+                        <CancelIcon style={{color: '#FFF'}}  />
+                      </IconButton></Tooltip>
+                    }
+                  </ListItemSecondaryAction>
+                </ListItem>
+              ))}
+            </List>
+          </TabPanel>
+          <TabPanel value={value} index={3}>
+            <List>
+              {viewProfileList.isUsers === true && viewProfileList.users.map((value) => (
+                <ListItem key={value.id} button>
+                  <ListItemText className={classes.margiin} id={value.id} >{value.firstname + '  ' + value.lastname}</ListItemText>
+                  <ListItemSecondaryAction>
+                    {value.like === null &&
+                      <Tooltip title="Like"><IconButton aria-label="Like" onClick={(e) => handleLike(value.id)}>
+                        <FavoriteBorderIcon color="secondary" />
+                      </IconButton></Tooltip>
+                    }
+                    {value.like === 'iLike' &&
+                      <Tooltip title="Unlike"><IconButton aria-label="Unlike" onClick={(e) => handleDislike(value.id)}>
+                        <FavoriteIcon color="secondary" />
+                      </IconButton></Tooltip>
+                    }
+                    {value.like === 'heLiked' &&
+                      <Tooltip title="Like back"><IconButton aria-label="Like back" onClick={(e) => handleLike(value.id)}>
+                        <LoyaltyIcon style={{color: '#FFF'}} />
+                      </IconButton></Tooltip>
+                    }
+                    {value.like === 'match' &&
+                      <Tooltip title="Unmatch"><IconButton aria-label="Unmatch" onClick={(e) => handleDislike(value.id)}>
+                        <CancelIcon style={{color: '#FFF'}}  />
+                      </IconButton></Tooltip>
+                    }
+                  </ListItemSecondaryAction>
+                </ListItem>
+              ))}
+            </List>
+          </TabPanel>
+        </Grid>
+      </Core.Paper>
+    </Grid>
+
+  )
 }
 export default Activity;
--- client/src/components/Cards/index.js
+++ client/src/components/Cards/index.js
@@ -6,6 +6,9 @@ import "./Cards.css";
 import { makeStyles, useTheme } from '@material-ui/core/styles';
 import SwipeableViews from 'react-swipeable-views';
 import VisibilityIcon from '@material-ui/icons/Visibility';
+import CancelIcon from '@material-ui/icons/Cancel';
+import LoyaltyIcon from '@material-ui/icons/Loyalty';
+
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -21,7 +24,7 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 export default function Cards(props) {
-  const { user, handleLike, handleDislike, handleReport, handleBlock, handleViewProfile} = props;
+  const { user, handleLike, handleDislike, handleReport, handleBlock, handleViewProfile } = props;
   const rating = user.user.rating;
   const classes = useStyles();
   const theme = useTheme();
@@ -30,7 +33,7 @@ export default function Cards(props) {
     setActiveStep(step);
   };
   return (
-    
+
     <div className="container">
       <Core.Card
         className="card"
@@ -53,16 +56,16 @@ export default function Cards(props) {
           avatar={
             user.images.length !== 0
               ? user.images.map((tile) => (
-                    <div key={tile.id}>
-                      {tile.isProfilePic === 1 && (
-                        <Core.Avatar
-                          style={{ border: "1px solid #c9c9c9" }}
-                          src={`http://localhost:3001/${tile.path}`}
-                        />
-                      )}
-                    </div>
-                  )
-                )
+                <div key={tile.id}>
+                  {tile.isProfilePic === 1 && (
+                    <Core.Avatar
+                      style={{ border: "1px solid #c9c9c9" }}
+                      src={`http://localhost:3001/${tile.path}`}
+                    />
+                  )}
+                </div>
+              )
+              )
               : null
           }
           title={user.user.username}
@@ -99,40 +102,40 @@ export default function Cards(props) {
         </div>
         <div className="cardAction">
           {/* {console.log(user)} */}
-        {user.user.like === null &&
-          <Core.Tooltip title="Like" >
-            <Core.IconButton aria-label="Like" onClick={(e) => handleLike(user.user.id)}>
-              <Icons.FavoriteBorder color="secondary" />
-            </Core.IconButton>
-          </Core.Tooltip>
+          {user.user.like === null &&
+            <Core.Tooltip title="Like" >
+              <Core.IconButton aria-label="Like" onClick={(e) => handleLike(user.user.id)}>
+                <Icons.FavoriteBorder style={{ color: '#34ada4' }} />
+              </Core.IconButton>
+            </Core.Tooltip>
           }
           {user.user.like === 'iLike' &&
-          <Core.Tooltip title ="Unlike"><Core.IconButton aria-label="Unlike" onClick={(e) => handleDislike(user.user.id)}>
-            <Icons.Favorite  color="secondary"/>
-          </Core.IconButton></Core.Tooltip>
-        }
-        {user.user.like === 'heLiked' &&
-          <Core.Tooltip title ="Like back"><Core.IconButton aria-label="Like back"  onClick={(e) => handleLike(user.user.id)}>
-            <Icons.SupervisedUserCircleOutlined color="primary"/>
-          </Core.IconButton></Core.Tooltip>
-        }
-        {user.user.like === 'match' &&
-          <Core.Tooltip title ="Unmatch"><Core.IconButton aria-label="Unmatch"  onClick={(e) => handleDislike(user.user.id)}>
-            <Icons.SupervisedUserCircleRounded color="primary"/>
-          </Core.IconButton></Core.Tooltip>
-        }
-          
+            <Core.Tooltip title="Unlike"><Core.IconButton aria-label="Unlike" onClick={(e) => handleDislike(user.user.id)}>
+              <Icons.Favorite style={{ color: '#34ada4' }}/>
+            </Core.IconButton></Core.Tooltip>
+          }
+          {user.user.like === 'heLiked' &&
+            <Core.Tooltip title="Like back"><Core.IconButton aria-label="Like back" onClick={(e) => handleLike(user.user.id)}>
+              <LoyaltyIcon style={{ color: '#FFF' }} />
+            </Core.IconButton></Core.Tooltip>
+          }
+          {user.user.like === 'match' &&
+            <Core.Tooltip title="Unmatch"><Core.IconButton aria-label="Unmatch" onClick={(e) => handleDislike(user.user.id)}>
+              <CancelIcon style={{ color: '#34ada4' }} />
+            </Core.IconButton></Core.Tooltip>
+          }
+
           <Core.Tooltip title="Block">
             <Core.IconButton aria-label="Block" onClick={(e) => handleBlock(user.user.id)}>
-              <Icons.Block color="secondary" />
+              <Icons.Block style={{ color: '#34ada4' }} />
             </Core.IconButton>
           </Core.Tooltip>
-          <Core.Tooltip title ="Report"><Core.IconButton aria-label="Report"  onClick={(e) => handleReport(user.user.id)}>
-        <Icons.Report  color="secondary"/>
-        </Core.IconButton></Core.Tooltip>
-          <Core.Tooltip title ="Report"><Core.IconButton aria-label="Report"  onClick={(e) => handleViewProfile(user.user.id)}>
-        <VisibilityIcon  color="secondary"/>
-        </Core.IconButton></Core.Tooltip>
+          <Core.Tooltip title="Report"><Core.IconButton aria-label="Report" onClick={(e) => handleReport(user.user.id)}>
+            <Icons.Report style={{ color: '#34ada4' }} />
+          </Core.IconButton></Core.Tooltip>
+          <Core.Tooltip title="ViewProfile"><Core.IconButton aria-label="Report" onClick={(e) => handleViewProfile(user.user.id)}>
+            <VisibilityIcon style={{ color: '#34ada4' }}/>
+          </Core.IconButton></Core.Tooltip>
         </div>
       </Core.Card>
     </div>
